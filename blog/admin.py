60c12124f05affbc0eaa01f3fd037d3f0b9d1abc
astatr1 <astatr1@yandex.ru>
2024-01-11 20:34:55 +0300

    Добавление и первоначальная настройка модуля администрирования для приложения blog.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'time_published', 'is_published']
+    list_filter = ['is_published', 'time_created', 'time_published', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'time_published'
+    ordering = ['is_published', 'time_published']
